8152cfc8702b942cae149c572ee1756b38b8b5cb
Adnan Ul Haque <117963791+AdnanUlHaque@users.noreply.github.com>
2023-02-03 20:53:53 +0530

    Create test4.java

---
+++ test4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Test4 {
+	public Test4() {
+	}
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		System.out.println("How Long is Ypor Brup ?");
+		int brupLength =sc.nextInt();
+		String msg="";
+		for(int i=1 ;i <= brupLength ; i++) {
+			msg=msg+"r";
+		}
+     System.out.println("Bu"+msg+"p");
+	}
+}
