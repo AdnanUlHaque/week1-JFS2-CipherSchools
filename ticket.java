a42ffd76ab66adc997f9b6fcde67bcefc760fc6b
Adnan Ul Haque <117963791+AdnanUlHaque@users.noreply.github.com>
2023-02-03 21:03:41 +0530

    Update and rename test5.java to ticket.java

--- ticket.java
+++ ticket.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 public class Tset5 {
 	public Tset5() {
-		// TODO Auto-generated constructor stub
 	}
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in); 
@@ -10,14 +9,13 @@ public class Tset5 {
 		System.out.println("Today is your birthday :");
 		boolean isbirhday=sc.nextBoolean();
 		if(isbirhday==true){
-      speed=5;
-    }
+			speed=5;
+		}
 		if(speed>80) 
 			System.out.printf("High ticket");
-    else if(speed>=60)
-      System.out.printf("Mid ticket"); 
-    else
-      System.out.printf("No ticket");
+		else if(speed>=60)
+			System.out.printf("Mid ticket"); 
+		else
+			System.out.printf("No ticket");
 	}
-
 }
