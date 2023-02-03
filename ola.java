32705c0fb5ed86ee94106c2ecb6d4abb2a61f3dc
Adnan Ul Haque <117963791+AdnanUlHaque@users.noreply.github.com>
2023-02-03 22:34:57 +0530

    Create ola.java

---
+++ ola.java
@@ -0,0 +1,37 @@
+package demo;
+public class Test1 {
+	public Test1() {
+  }
+  interface olaCustomer{
+    void bookCab();
+    void rentCab();
+		void findARide();
+		void rateDriver();
+    void scheduleCab(); 
+    void  cancelCab();
+    void addMoneytowallet();
+    void raiseTicket();
+    void signup(); 
+    void signIn();
+  }
+  interface olaDriver{
+    void acceptBooking();
+		void getCustomerLocation();
+    void setAvailableStatus(); 
+    void getMyCommission();
+    void raiseMoneyTransferRequest();
+    void setUpYourVehicle();
+    void callCustomer();
+    void customerFeedback();
+  }
+  interface olaOwner{
+    void getDriverRecords();
+		void getReportByDriverID();
+    void getReportByegion(); 
+    void getTotalRevenue();
+    void getcustomerFeedback();
+    void addDriver();
+    void removeDriver();
+    void changeDriverStatus();
+  }
+}
