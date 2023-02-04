366b2d33e2e2c0a1b303d850f0c201cf1a1b3bc4
Adnan Ul Haque <117963791+AdnanUlHaque@users.noreply.github.com>
2023-02-04 22:38:19 +0530

    Update burp.java

--- burp.java
+++ burp.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 public class Test4 {
-	public Test4() {
-	}
 	public static void main(String[] args) {
 		Scanner sc =new Scanner(System.in);
 		System.out.println("How Long is Ypor Brup ?");
